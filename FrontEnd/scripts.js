a2278dd3baaf19e88c56d0f1d58713c27dde71ae
Riley Bates <rbates@smu.edu>
2019-03-28 18:50:42 -0500

    Created standard react app

--- FrontEnd/scripts.js
+++
@@ -1,7 +0,0 @@
-function redirect() {
-    location.replace("file:///Users/rbates/Documents/SMU/Spring%202019/UI/car_app/welcome.html")
-}
-
-function logout() {
-    location.replace("file:///Users/rbates/Documents/SMU/Spring%202019/UI/car_app/index.html")
-}
