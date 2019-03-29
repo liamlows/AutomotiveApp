64987bb09646b16e91b3764ee16a85a2fcc5d146
Riley Bates <rbates@smu.edu>
2019-03-28 19:13:45 -0500

    added bootstrap, jquery, font-awesome dependencies

--- FrontEnd/car_app/src/index.js
+++ FrontEnd/car_app/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from './serviceWorker';
+import 'bootstrap';
+import 'font-awesome/css/font-awesome.css'
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
