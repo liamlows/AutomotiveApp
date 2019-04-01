6fae0e740ed933ca344e99b00c07915750a4586a
RuichenNi <31213944+RuichenNi@users.noreply.github.com>
2019-04-01 16:30:07 -0500

    Add files via upload

--- FrontEnd/car_app/src/App.js
+++ FrontEnd/car_app/src/App.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Insurance} from './Insurance';
+import 'bootstrap/dist/css/bootstrap.css';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faEnvelope, faCar, faKey, faBuilding } from '@fortawesome/free-solid-svg-icons';
 
+library.add(faEnvelope, faCar, faKey, faBuilding);
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        Car stuff
-      </div>
+      <>
+        <Insurance />
+      </>
     );
   }
 }
