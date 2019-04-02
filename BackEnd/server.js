d17960b40da4b4e9b6c1473eafbbed56f68a77c6
liamlows <liam.lowsleywilliams@gmail.com>
2019-04-02 01:25:24 -0500

    accident v2.1 :(

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -5,7 +5,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT || 8080;
-const HOST = 0.0.0.0;
+const HOST = '0.0.0.0';
 
 // Start API server
 app.listen(PORT, HOST);
