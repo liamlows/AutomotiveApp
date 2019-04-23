082f6d9e8f2ba89f59b729d45110c5adea8d5782
liamlows <liam.lowsleywilliams@gmail.com>
2019-04-23 15:54:21 -0500

    finalized all car and user endpoints, branching & merging to test with maintance endpoints

---
+++ BackEnd/NODE/App/controller/appControllerUser.js
@@ -0,0 +1,330 @@
+'use strict';
+var User = require('../model/appUser.js');
+
+// Register user
+exports.create_user = function(req,res){
+  // gen user obj
+  var new_user = new User(req.body);
+  // handles empty input
+  if (!new_user.email || !new_user.password || !new_user.first_name || !new_user.last_name) {
+    res.status(400).json({
+      "code": 400,
+      "response":"Please provide input for all fields."
+    });
+  }
+  else{
+    // if input is provided, create user
+    User.createUser(new_user, function(err,user){
+      if (err){
+        res.send(err);
+      }
+      else{
+        res.json(user);
+      }
+    });
+  }
+};
+// Login user
+exports.login_user = function(req,res){
+  // gen user obj
+  var login_user = new User(req.body);
+  // handles empty input
+  if(!login_user.email || !login_user.password){
+    res.status(400).json({
+      "code": 400,
+      "response":"Please provide input for all fields."
+    });
+  }
+  else{
+    // if input is provided, login user
+    User.loginUser(login_user, function(err, user){
+      if(err){
+        res.json(err);
+      }
+      else {
+        res.json(user);
+      }
+    });
+  }
+};
+// Delete user account
+exports.delete_user = function(req,res){
+  if(!req.params.id){
+    res.status(400).json({
+      "code": 400,
+      "response":"Missing ID in API request."
+    });
+  }
+  else{
+    User.deleteUserByID(req.params.id, function(err,user){
+      if (err){
+        res.send(err);
+      }
+      else{
+        res.json(user);
+      }
+    });
+  }
+};
+// Update user email
+exports.update_email = function(req,res){
+  // handle missing ID num
+  if(!req.params.id){
+    res.status(400).json({
+      "code": 400,
+      "response":"Missing ID in API request."
+    });
+  }
+  // handle missing email from body
+  else if(!req.body.email){
+    res.status(400).json({
+      "code": 400,
+      "response":"Missing email in API request body."
+    });
+  }
+  else{
+    // update email function
+    User.updateEmailByID(req.params.id, req.body.email, function(err,user){
+      if(err){
+        res.send(err);
+      }
+      else {
+        res.json(user);
+      }
+    });
+  }
+};
+// Update user first name
+exports.update_fname = function(req,res){
+  // handle missing ID
+  if(!req.params.id){
+    res.status(400).json({
+      "code": 400,
+      "response":"Missing ID in API request."
+    });
+  }
+  // handle missing first name
+  else if(!req.body.first_name){
+    res.status(400).json({
+      "code": 400,
+      "response":"Missing first name in API request body."
+    });
+  }
+  else{
+    // update name function
+    User.updateFNameByID(req.params.id, req.body.first_name, function(err,user){
+      if(err){
+        res.send(err);
+      }
+      else {
+        res.json(user);
+      }
+    });
+  }
+};
+// Update user last name
+exports.update_lname = function(req,res){
+  // handle missing ID
+  if(!req.params.id){
+    res.status(400).json({
+      "code": 400,
+      "response":"Missing ID in API request."
+    });
+  }
+  // handle missing last name
+  else if(!req.body.last_name){
+    res.status(400).json({
+      "code": 400,
+      "response":"Missing last name in API request body."
+    });
+  }
+  else{
+    // update last name function
+    User.updateLNameByID(req.params.id, req.body.last_name, function(err,user){
+      if(err){
+        res.send(err);
+      }
+      else {
+        res.json(user);
+      }
+    });
+  }
+};
+// Update user password
+exports.update_pass = function(req,res){
+  // handle missing ID
+  if(!req.params.id){
+    res.status(400).json({
+      "code": 400,
+      "response":"Missing ID in API request."
+    });
+  }
+  // handle missing password
+  else if(!req.body.password){
+    res.status(400).json({
+      "code": 400,
+      "response":"Missing password in API request body."
+    });
+  }
+  else{
+    // update password hash func
+    User.updatePassByID(req.params.id, req.body.password, function(err,user){
+      if(err){
+        res.send(err);
+      }
+      else {
+        res.json(user);
+      }
+    });
+  }
+};
+// Update/remove favorite shop name
+exports.update_shop_name = function(req,res){
+  if(!req.params.id){
+    res.status(400).json({
+      "code": 400,
+      "response":"Missing ID in API request."
+    });
+  }
+  else if(!req.body.shop_name){
+    res.status(400).json({
+      "code": 400,
+      "response":"Missing Shop in API request."
+    });
+  }
+  else{
+    User.updateShopNameByID(req.body.shop_name, req.params.id, function(err,user){
+      if (err){
+        res.send(err);
+      }
+      else{
+        res.json(user);
+      }
+    });
+  }
+};
+// Update/remove favorite shop phone
+exports.update_shop_phone = function(req,res){
+  if(!req.params.id){
+    res.status(400).json({
+      "code": 400,
+      "response":"Missing ID in API request."
+    });
+  }
+  else if(!req.body.shop_phone){
+    res.status(400).json({
+      "code": 400,
+      "response":"Missing Shop in API request."
+    });
+  }
+  else{
+    User.updateShopNameByID(req.body.shop_phone, req.params.id, function(err,user){
+      if (err){
+        res.send(err);
+      }
+      else{
+        res.json(user);
+      }
+    });
+  }
+};
+// Update/remove insurance company
+exports.update_insurance_company = function(req,res){
+  if(!req.params.id){
+    res.status(400).json({
+      "code": 400,
+      "response":"Missing ID in API request."
+    });
+  }
+  else if(!req.body.insurance_company){
+    res.status(400).json({
+      "code": 400,
+      "response":"Missing Shop in API request."
+    });
+  }
+  else{
+    User.updateInsuranceCompanyByID(req.body.insurance_company, req.params.id, function(err,user){
+      if (err){
+        res.send(err);
+      }
+      else{
+        res.json(user);
+      }
+    });
+  }
+};
+// Update/remove insurance phone
+exports.update_insurance_phone = function(req,res){
+  if(!req.params.id){
+    res.status(400).json({
+      "code": 400,
+      "response":"Missing ID in API request."
+    });
+  }
+  else if(!req.body.insurance_phone){
+    res.status(400).json({
+      "code": 400,
+      "response":"Missing Shop in API request."
+    });
+  }
+  else{
+    User.updateInsuranceCompanyByID(req.body.insurance_phone, req.params.id, function(err,user){
+      if (err){
+        res.send(err);
+      }
+      else{
+        res.json(user);
+      }
+    });
+  }
+};
+// Update/remove insurance email
+exports.update_insurance_email = function(req,res){
+  if(!req.params.id){
+    res.status(400).json({
+      "code": 400,
+      "response":"Missing ID in API request."
+    });
+  }
+  else if(!req.body.insurance_email){
+    res.status(400).json({
+      "code": 400,
+      "response":"Missing Shop in API request."
+    });
+  }
+  else{
+    User.updateInsuranceCompanyByID(req.body.insurance_email, req.params.id, function(err,user){
+      if (err){
+        res.send(err);
+      }
+      else{
+        res.json(user);
+      }
+    });
+  }
+};
+// Update/remove insurance number
+exports.update_insurance_num = function(req,res){
+  if(!req.params.id){
+    res.status(400).json({
+      "code": 400,
+      "response":"Missing ID in API request."
+    });
+  }
+  else if(!req.body.insurance_num){
+    res.status(400).json({
+      "code": 400,
+      "response":"Missing Shop in API request."
+    });
+  }
+  else{
+    User.updateInsuranceCompanyByID(req.body.insurance_num, req.params.id, function(err,user){
+      if (err){
+        res.send(err);
+      }
+      else{
+        res.json(user);
+      }
+    });
+  }
+};
--- BackEnd/NODE/App/routes/appRoutes.js
+++ BackEnd/NODE/App/routes/appRoutes.js
@@ -1,43 +1,61 @@
 'use strict';
 module.exports = function(app) {
-  var autoApp = require('../controller/appController');
+  var autoAppUser = require('../controller/appControllerUser');
+  var autoAppCar = require('../controller/appControllerVehicle');
 
-  // REGISTER
+
+  // REGISTER USER
   app.route('/register')
-    .post(autoApp.create_user);
-  // LOGIN
+    .post(autoAppUser.create_user);
+  // LOGIN USER
   app.route('/login')
-    .post(autoApp.login_user);
-  // UPDATE
-  app.route('/user/email/:id')
-    .put(autoApp.update_email);
-  app.route('/user/first_name/:id')
-    .put(autoApp.update_fname);
-  app.route('/user/last_name/:id')
-    .put(autoApp.update_lname);
-  app.route('/user/password/:id')
-    .put(autoApp.update_pass);
-  // Car
-  app.route('/car/add_car/:id')
-    .post(autoApp.add_car);
-  app.route('/car/get_car/:id')
-    .get(autoApp.get_car);
-
-
-//Delete user account
+    .post(autoAppUser.login_user);
+  // DELETE USER
   app.route('/delete_user/:id')
-    .delete(autoApp.delete_user);
+    .delete(autoAppUser.delete_user);
+  // UPDATE USER
+  app.route('/user/email/:id')
+    .put(autoAppUser.update_email);
+  app.route('/user/first_name/:id')
+    .put(autoAppUser.update_fname);
+  app.route('/user/last_name/:id')
+    .put(autoAppUser.update_lname);
+  app.route('/user/password/:id')
+    .put(autoAppUser.update_pass);
+  app.route('/user/update_shop_name/:id')
+    .put(autoAppUser.update_shop_name);
+  app.route('/user/update_shop_phone/:id')
+    .put(autoAppUser.update_shop_phone);
+  app.route('/user/update_insurance_company/:id')
+    .put(autoAppUser.update_insurance_company);
+  app.route('/user/update_insurance_phone/:id')
+    .put(autoAppUser.update_insurance_phone);
+  app.route('/user/update_insurance_email/:id')
+    .put(autoAppUser.update_insurance_email);
+  app.route('/user/update_insurance_num/:id')
+    .put(autoAppUser.update_insurance_num);
 
-//Set/remove favorite shop name and phone
-  app.route('/user/update_shop/:id')
-    .put(autoApp.update_shop);
-  app.route('/user/remove_shop/:id')
-    .put(autoApp.remove_shop);
-    
-//Set/remove insurance name and policy
-  app.route('/user/update_insurance/:id')
-    .put(autoApp.update_insurance);
-  app.route('/user/remove_insurance/:id')
-    .put(autoApp.remove_insurance);
+  // ADD CAR
+  app.route('/car/add_car/:id')
+    .post(autoAppCar.add_car);
+  // GET CAR
+  app.route('/car/get_car/:id')
+    .get(autoAppCar.get_car);
+  // DELETE CAR
+  app.route('/car/delete_car/:id')
+    .delete(autoAppCar.delete_car)
+  // UPDATE CAR
+  app.route('/car/update_car_all/:id')
+    .put(autoAppCar.update_car_all)
+  app.route('/car/update_car_make/:id')
+    .put(autoAppCar.update_car_make)
+  app.route('/car/update_car_model/:id')
+    .put(autoAppCar.update_car_model)
+  app.route('/car/update_car_year/:id')
+    .put(autoAppCar.update_car_year)
+  app.route('/car/update_car_avg_mileage/:id')
+    .put(autoAppCar.update_car_avg_mileage)
+  app.route('/car/update_car_cur_mileage/:id')
+    .put(autoAppCar.update_car_cur_mileage)
 
 };
