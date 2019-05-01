b0284e0bc5b47568a3c445638ce100ea71ac4b5a
Julie Tapia <jtapia@smu.edu>
2019-04-30 21:26:01 -0500

    fix merge

--- FrontEnd/car_app/src/app/accountInfo/AccountInfo.jsx
+++ FrontEnd/car_app/src/app/accountInfo/AccountInfo.jsx
@@ -1,12 +1,6 @@
 import React, { Component } from "react";
-<<<<<<< HEAD
-import { User } from './../../models/user';
-import InsurancePage from './InsurancePage';
-import MyNav from '../home/MyNav'
-=======
 import InsurancePage from './InsurancePage'
 import MyNav from "../home/MyNav";
->>>>>>> 227e9c644447ac850f97846f549548eb846b5460
 
 class AccountInfo extends Component {
 
@@ -17,11 +11,7 @@ class AccountInfo extends Component {
   render() {
     return (
       <div className="">
-<<<<<<< HEAD
-      <MyNav /> 
-=======
         <MyNav />
->>>>>>> 227e9c644447ac850f97846f549548eb846b5460
         <InsurancePage />
       </div>
     );
